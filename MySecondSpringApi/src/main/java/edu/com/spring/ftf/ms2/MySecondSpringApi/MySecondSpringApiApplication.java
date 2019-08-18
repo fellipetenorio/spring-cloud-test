c4acad98e9c19b8516acce6408c22c80796c79da
ft <ftf@ftf.com>
2019-08-18 16:52:59 -0300

    second service registering in discovery service

--- MySecondSpringApi/src/main/java/edu/com/spring/ftf/ms2/MySecondSpringApi/MySecondSpringApiApplication.java
+++ MySecondSpringApi/src/main/java/edu/com/spring/ftf/ms2/MySecondSpringApi/MySecondSpringApiApplication.java
@@ -2,12 +2,24 @@ package edu.com.spring.ftf.ms2.MySecondSpringApi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MySecondSpringApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MySecondSpringApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MySecondSpringApiApplication.class, args);
+    }
 
+}
+
+@RestController
+class ServiceInstanceRestController {
+    @GetMapping({"/", "/status"})
+	public String status() {
+        return "ok";
+    }
 }
